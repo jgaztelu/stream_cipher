bdff3e65df13c649cec32daab88447a33e33e1bf
Javier Gaztelumendi <jgazte@gmail.com>
2017-04-06 17:12:29 +0200

    FSR component with generics completed
    The FSR component is a Feedback Shift Register which can be configured
    when instantiated.
    It has a feedback and step input, as well as initial value.
    The outputs are the last bits and the taps for the external feedback
    function.

--- FSR.vhdl
+++ FSR.vhdl
@@ -1,26 +1,29 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
+  use work.fsr_taps_type.all;
+  entity FSR is
 
-entity FSR is
-  generic (
-  r_WIDTH  : integer :=  128;
-  r_STEP   : integer := 1;
-  r_FWIDTH  : integer := 6
-  );
-  port (
-  clk      : in std_logic;
-  rst      : in std_logic;
-  feedback : in std_logic_vector ((r_STEP-1) downto 0);
-  init     : in std_logic;
-  ini_data : in std_logic_vector ((r_WIDTH-1) downto 0);
-  out_data : out std_logic_vector ((r_STEP-1) downto 0);
-  fb_out   : out std_logic_vector ((r_FWIDTH-1) downto 0)
-  );
-end entity;
+    generic (
+    r_WIDTH  : integer :=  128;
+    r_STEP   : integer := 1;
+    r_FWIDTH  : integer := 6;
+    r_TAPS    : TAPS (0 to 31):= (32,47,58,90,121,128,others=>-1)           -- Change the size according to the number of taps
+    );
 
-architecture behavioural of FSR is
-signal shifted,shifted_next  : std_logic_vector((r_WIDTH-1) downto 0);
+    port (
+    clk      : in std_logic;
+    rst      : in std_logic;
+    feedback : in std_logic_vector ((r_STEP-1) downto 0);
+    init     : in std_logic;
+    ini_data : in std_logic_vector ((r_WIDTH-1) downto 0);
+    out_data : out std_logic_vector ((r_STEP-1) downto 0);
+    fb_out   : out std_logic_vector ((r_FWIDTH-1) downto 0)
+    );
+  end entity;
+
+    architecture behavioural of FSR is
+    signal shifted,shifted_next  : std_logic_vector((r_WIDTH-1) downto 0);
 begin
 process(clk,rst)
 begin
@@ -31,7 +34,7 @@ elsif clk'event and clk = '1' then
 end if;
 end process;
 
-process (feedback,init,ini_data)
+process (feedback,init,ini_data,shifted)
 begin
   if init = '1' then
     shifted_next <= ini_data;
@@ -39,10 +42,10 @@ begin
     shifted_next <= shifted((r_WIDTH-r_STEP-1) downto 0) & feedback;
   end if;
 end process;
-out_data <= shifted ((r_WIDTH-1) downto (r_WIDTH-r_STEP-1));
+out_data <= shifted ((r_WIDTH-1) downto (r_WIDTH-r_STEP));
 
-gen_feedback: for I in r_FWIDTH to 0 generate
-  fb_out(I) <= shifted (I);
-end generate gen_feedback;
+    gen_feedback: for I in 0 to (r_FWIDTH-1) generate
+      fb_out(I) <= shifted(r_TAPS(I)-1);
+    end generate gen_feedback;
 
 end architecture;
---
+++ FSR_pkg.vhdl
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------
+--  Package: fsr_taps_type
+--  Content: This package contains the type to define the taps of a
+--           Feedback Shift Register before synthesis.
+-----------------------------------------------------------------------------
+
+library IEEE;
+  use ieee.std_logic_1164.all;
+
+package fsr_taps_type is
+    type TAPS is array (integer range <>) of integer;
+end package fsr_taps_type;

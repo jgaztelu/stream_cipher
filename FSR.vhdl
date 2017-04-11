d39bd7787bce6dbab2a5fdee7a0f15336c0ffc74
Javier Gaztelumendi <jgazte@gmail.com>
2017-04-11 17:03:51 +0200

    Added extra FSR outputs for the h function inputs

--- FSR.vhdl
+++ FSR.vhdl
@@ -8,7 +8,9 @@ library ieee;
     r_WIDTH  : integer :=  128;
     r_STEP   : integer := 1;
     r_FWIDTH  : integer := 6;
-    r_TAPS    : TAPS (0 to 31):= (128,121,90,58,47,32,others=>-1)           -- Change the size according to the number of taps
+    r_HWIDTH  : integer := 2;
+    r_TAPS    : TAPS (0 to 31):= (128,121,90,58,47,32,others=>-1);           -- Change the size according to the number of taps
+    r_STATE   : TAPS (0 to 15):= (116,33, others => -1)
     );
 
     port (
@@ -18,7 +20,8 @@ library ieee;
     init     : in std_logic;
     ini_data : in std_logic_vector ((r_WIDTH-1) downto 0);
     out_data : out std_logic_vector ((r_STEP-1) downto 0);
-    fb_out   : out std_logic_vector ((r_FWIDTH-1) downto 0)
+    fb_out   : out std_logic_vector ((r_FWIDTH-1) downto 0);
+    h_out    : out std_logic_vector ((r_HWIDTH-1) downto 0)
     );
   end entity;
 
@@ -48,5 +51,10 @@ out_data <= shifted ((r_WIDTH-1) downto (r_WIDTH-r_STEP));
     gen_feedback: for I in 0 to (r_FWIDTH-1)  generate
       fb_out(I) <= shifted(r_TAPS(I)-1);
     end generate gen_feedback;
+
+-- Connect output bits for h function
+    gen_h: for I in 0 to (r_HWIDTH-1)  generate
+      h_out(I) <= shifted(r_STATE(I)-1);
+    end generate gen_h;
 
 end architecture;

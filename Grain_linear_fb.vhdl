3dc1e0165d255f5e6a82fe68f76cd40b2096e0ef
Javier Gaztelumendi <jgazte@gmail.com>
2017-04-11 14:27:45 +0200

    Added initialization input to Feedback functions

--- Grain_linear_fb.vhdl
+++ Grain_linear_fb.vhdl
@@ -4,14 +4,21 @@ library ieee;
 
 entity grain_linear_fb is
   port (
-  taps_in : in std_logic_vector  (5 downto 0);
-  fb_out  : out std_logic
+  taps_in    : in std_logic_vector  (5 downto 0);
+  pre_out_in : in std_logic;
+  init       : in std_logic;
+  fb_out     : out std_logic
   );
 end entity;
 
 architecture linear_fb of grain_linear_fb is
 begin
-
-fb_out <= taps_in(5) xor taps_in(4) xor taps_in(3) xor taps_in(2) xor taps_in(1) xor taps_in(0) xor '1';
-
+process(taps_in,pre_out_in,init)
+begin
+if init = '1' then
+  fb_out <= pre_out_in xor taps_in(5) xor taps_in(4) xor taps_in(3) xor taps_in(2) xor taps_in(1) xor taps_in(0) xor '1';
+else
+  fb_out <= taps_in(5) xor taps_in(4) xor taps_in(3) xor taps_in(2) xor taps_in(1) xor taps_in(0) xor '1';
+end if;
+end process;
 end architecture;
